79c67d9de41f0306111e2efe8e95a191b686a0ab
ofunnelonline <ofunnelonline@gmail.com>
2025-06-09 22:02:09 +0200

    Add files via upload

---
+++ OFv1/config.js
@@ -0,0 +1,19 @@
+window.modelConfig = {
+  name: "Test Person",
+  handle: "@testperson",
+  locationFallback: "Miami",
+  profileImage: "images/profile.jpg",
+  teaseImages: [
+    { src: "images/tease1.jpg", label: "Subscriber Special 🔒" },
+    { src: "images/tease2.jpg", label: "POV Tease 🔒" }
+  ],
+  vaultOfferText: "🎉 55% OFF for 24 Hours Only",
+  vaultOfferSub: "Unlock my private vault and save big – offer expires soon!",
+  ctaLink: "https://onlyfans.com",
+  testimonials: [
+    { initials: "JD", text: "Not gonna lie, this is the best thing I've ever seen in my life, I don't regret it at all", joined: "3 months ago" },
+    { initials: "MK", text: "Thanks for the special twerking video, even saying my name", joined: "1 month ago" },
+    { initials: "LS", text: "Your teasing style is so fun I want to be here all day", joined: "7 months ago" },
+    { initials: "AV", text: "Best sub I’ve ever spent my money on I wish I could give you more", joined: "5 months ago" }
+  ]
+};
